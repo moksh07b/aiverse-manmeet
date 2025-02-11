9518fd8a4c4261a983c4ebdba9a1a5ecf7cd974c
Manmeet Kaur <manmeetkaur@Manmeets-MacBook-Air.local>
2025-02-11 22:30:09 +0530

    update pom.xml

--- src/main/java/ai/verse/AIVerseApplication.java
+++ src/main/java/ai/verse/AIVerseApplication.java
@@ -1,11 +1,7 @@
-package ai.verse;
+package com.aiverse;
 
-import ai.verse.repo.PostRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
@@ -14,5 +10,4 @@ public class AIVerseApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AIVerseApplication.class, args);
 	}
-
 }
--- src/test/java/com/aiverse/AiverseApplicationTests.java
+++ src/test/java/com/aiverse/AiverseApplicationTests.java
@@ -3,11 +3,12 @@ package com.aiverse;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
 class AiverseApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// This test verifies the Spring context loads successfully
 	}
-
 }
